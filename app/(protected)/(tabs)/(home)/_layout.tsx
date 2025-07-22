e1e54560a71c3883f0944f2b2f7c36913f85c614
shalqahtani <eng.shahad.alqahtani@gmail.com>
2025-07-22 19:40:01 +0300

    Updated translations

--- app/(protected)/(tabs)/(home)/_layout.tsx
+++ app/(protected)/(tabs)/(home)/_layout.tsx
@@ -1,24 +1,28 @@
-
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack  screenOptions={{headerShown: true,
-        headerStyle: { backgroundColor: "#047e57",borderBottomWidth: 0
-
-         },
-        headerTintColor: "#ffffff",headerBackVisible: false, title: "",
-       // headerBackButtonDisplayMode: "minimal",
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        headerStyle: { backgroundColor: "#047e57", borderBottomWidth: 0 }, //here code
+        headerTintColor: "#ffffff",
+        headerBackVisible: false,
+        title: "",
+        headerBackButtonDisplayMode: "minimal",
         headerTitleStyle: { color: "#ffffff" },
-      }}>
-        <Stack.Screen
+      }}
+    >
+      <Stack.Screen
         name="/(protected)/(tabs)/(home)"
-        options={{headerShown: false,
+        options={{
+          headerShown: false,
           headerBackVisible: false,
           gestureEnabled: false,
           headerTitle: "Home",
         }}
       />
-        {/* <Slot /> */}
-      </Stack>;
-
+      {/* <Slot /> */}
+    </Stack>
+  );
 }
--- app/(protected)/(tabs)/(home)/receive.tsx
+++ app/(protected)/(tabs)/(home)/receive.tsx
@@ -1,5 +1,6 @@
 import { collect, fetchItemsByType } from "@/api/items";
 import AuthContext from "@/context/AuthContext";
+import useT from "@/utils/useT";
 import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { useFocusEffect, useLocalSearchParams } from "expo-router";
@@ -11,15 +12,20 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
 } from "react-native";
-import useT from "@/utils/useT";
+// import placeholderImage from "../../../../assets/images/placeholder.png";
+
+const placeholderImage = require("../../../../images/IMG-20250707-WA0011.jpg");
 
 export default function Receive() {
   const { type } = useLocalSearchParams();
-  const capitalizedType = type?.toString().charAt(0).toUpperCase() + type?.toString().slice(1);
+  const capitalizedType =
+    type?.toString().charAt(0).toUpperCase() + type?.toString().slice(1);
   const getLatLongFromLocation = (locationString: string) => {
-    const match = locationString.match(/Lat:\s*(-?\d+(\.\d+)?),\s*Lon:\s*(-?\d+(\.\d+)?)/);
+    const match = locationString.match(
+      /Lat:\s*(-?\d+(\.\d+)?),\s*Lon:\s*(-?\d+(\.\d+)?)/
+    );
     if (!match) return null;
     const lat = parseFloat(match[1]);
     const lon = parseFloat(match[3]);
@@ -71,9 +77,13 @@ export default function Receive() {
     },
   });
 
+  console.log("recived data :", items);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{t("collect")} {t(type as "clothes" | "food" | "furniture")}</Text>
+      <Text style={styles.title}>
+        {t("collect")} {t(type as "clothes" | "food" | "furniture")}
+      </Text>
       <FlatList
         data={items}
         keyExtractor={(item) => item._id}
@@ -83,30 +93,54 @@ export default function Receive() {
           <View style={styles.card}>
             <View style={styles.cardLeft}>
               <Text style={styles.badge}>{t("tender")}</Text>
-              <Text style={styles.textLabel}>{t("details")}: {item.details}</Text>
-              <Text style={styles.textLabel}>{t("quantity")}: {item.quantity}</Text>
-              <Text style={styles.textLabel}>{t("address")}: {item.address}</Text>
-              <Text style={styles.textLabel}>{t("location")}: {item.location}</Text>
-              <Text style={styles.textLabel}>{t("phone")}: {item.contact}</Text>
+              <Text style={styles.textLabel}>
+                {t("details")}: {item.details}
+              </Text>
+              <Text style={styles.textLabel}>
+                {t("quantity")}: {item.quantity}
+              </Text>
+              <Text style={styles.textLabel}>
+                {t("address")}: {item.address}
+              </Text>
+              <Text style={styles.textLabel}>
+                {t("location")}: {item.location}
+              </Text>
+              <Text style={styles.textLabel}>
+                {t("phone")}: {item.contact}
+              </Text>
 
               {/* Call Button */}
               <Text
                 style={styles.linkButton}
                 onPress={() =>
-                  Linking.openURL(`tel:${item.contact.length === 8 ? `+965${item.contact}` : item.contact}`)
+                  Linking.openURL(
+                    `tel:${
+                      item.contact.length === 8
+                        ? `+965${item.contact}`
+                        : item.contact
+                    }`
+                  )
                 }
               >
-                <MaterialIcons name="call" size={20} color="#047e57" /> {t("call")}
+                <MaterialIcons name="call" size={20} color="#047e57" />{" "}
+                {t("call")}
               </Text>
 
               {/* WhatsApp Button */}
               <Text
                 style={styles.linkButton}
                 onPress={() =>
-                  Linking.openURL(`https://wa.me/${item.contact.length === 8 ? `965${item.contact}` : item.contact}`)
+                  Linking.openURL(
+                    `https://wa.me/${
+                      item.contact.length === 8
+                        ? `965${item.contact}`
+                        : item.contact
+                    }`
+                  )
                 }
               >
-                <FontAwesome name="whatsapp" size={20} color="#25D366" /> {t("whatsapp")}
+                <FontAwesome name="whatsapp" size={20} color="#25D366" />{" "}
+                {t("whatsapp")}
               </Text>
               {item.location && getLatLongFromLocation(item.location) && (
                 <Text
@@ -114,18 +148,26 @@ export default function Receive() {
                   onPress={() => {
                     const coords = getLatLongFromLocation(item.location);
                     if (coords) {
-                      Linking.openURL(`https://www.google.com/maps/search/?api=1&query=${coords.lat},${coords.lon}`);
+                      Linking.openURL(
+                        `https://www.google.com/maps/search/?api=1&query=${coords.lat},${coords.lon}`
+                      );
                     }
                   }}
                 >
-                  <MaterialIcons name="location-on" size={20} color="#f44336" /> {t("viewOnMap")}
+                  <MaterialIcons name="location-on" size={20} color="#f44336" />{" "}
+                  {t("viewOnMap")}
                 </Text>
               )}
             </View>
 
             <View style={styles.cardRight}>
               {item.image && (
-                <Image source={{ uri: item.image }} resizeMode="contain" style={styles.cardImage} />
+                <Image
+                  //  source={{ uri: item.image }}
+                  source={item.image ? { uri: item.image } : placeholderImage}
+                  resizeMode="contain"
+                  style={styles.cardImage}
+                />
               )}
 
               <TouchableOpacity
@@ -136,7 +178,9 @@ export default function Receive() {
                     receiver: user?._id as string,
                     itemType: type as string,
                     itemId: item._id,
-                    message: `${user?.email as string} ${t("collectedMsg")} ${item.details}`,
+                    message: `${user?.email as string} ${t("collectedMsg")} ${
+                      item.details
+                    }`,
                   })
                 }
               >
@@ -152,25 +196,25 @@ export default function Receive() {
 }
 
 const styles = StyleSheet.create({
-cardRight: {
-  justifyContent: "space-between",
-  alignItems: "center",
-},
-collectButton: {
-  flexDirection: "row",
-  alignItems: "center",
-  backgroundColor: "#047e57",
-  paddingVertical: 6,
-  paddingHorizontal: 10,
-  borderRadius: 6,
-  marginTop: 8,
-},
-collectButtonText: {
-  color: "#fff",
-  fontSize: 14,
-  marginLeft: 6,
-  fontWeight: "600",
-},
+  cardRight: {
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  collectButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#047e57",
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    borderRadius: 6,
+    marginTop: 8,
+  },
+  collectButtonText: {
+    color: "#fff",
+    fontSize: 14,
+    marginLeft: 6,
+    fontWeight: "600",
+  },
 
   container: { flex: 1, backgroundColor: "#e9f5f1" },
   header: {
@@ -181,13 +225,13 @@ collectButtonText: {
     borderBottomRightRadius: 24,
   },
   linkButton: {
-  fontSize: 14,
-  color: "#047e57",
-  marginTop: 4,
-  textDecorationLine: "underline",
-},
+    fontSize: 14,
+    color: "#047e57",
+    marginTop: 4,
+    textDecorationLine: "underline",
+  },
 
-    title: {
+  title: {
     fontSize: 26,
     fontWeight: "bold",
     paddingTop: 10,
